5a2c2234606e2e57bffde4b220170e4ba48837f4
vamshi bembiri <vbembiri@gmail.com>
2021-10-05 16:27:14 -0400

    Initial commit

---
+++ sql1.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Table, Column, Integer, String, MetaData
+meta = MetaData()
+
+students = Table(
+   'students', meta, 
+   Column('id', Integer, primary_key = True), 
+   Column('name', String), 
+   Column('lastname', String), 
+)
